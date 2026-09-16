a77fad5fb0cd16dc09ffaaa5f18bcd2b0dc8e266
User <ryan.sissom@yahoo.com>
2026-09-16 18:24:37 -0500

    Refactor main function to use logger middleware and improve request logging

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ryansissom/go-rest-api/internal/logger"
 	"github.com/ryansissom/go-rest-api/internal/router"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
-	logger.Info("server starting on port 8080")
-	if err := http.ListenAndServe(":8080", router.New()); err != nil {
-		logger.Error("Failed to start server", "error", err)
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+
+	r := router.New()
+	wrappedRouter := logger.AddLoggerMid(log, logger.LoggerMid(r))
+
+	log.Info("server starting on port 8080")
+
+	if err := http.ListenAndServe(":8080", wrappedRouter); err != nil {
+		log.Error("Failed to start server", "error", err)
 	}
 }
